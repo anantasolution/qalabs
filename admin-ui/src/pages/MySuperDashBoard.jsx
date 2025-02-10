91172d4b72dfc2618d65d716730ec9fe5ebfb66b
SaurabhMagara <saurabhmagara@gmail.com>
2025-02-10 16:37:52 +0530

    Navbar side navbar added

---
+++ admin-ui/src/pages/MySuperDashBoard.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+import { Outlet } from 'react-router-dom';
+
+//Importing components
+import Navbar from '../components/Navbar';
+import SideNavbar from '../components/SideNavbar';
+
+
+function MySuperDashboard() {
+
+    const [toggle, setToggle] = useState(true)
+
+    return (
+        <div className='max-w-screen h-[100vh] flex'>
+            {/* Side navbar */}
+            <SideNavbar toggle={toggle} setToggle={setToggle}></SideNavbar>
+            <div className={`flex ${toggle ? "w-[83%]" : "w-[94%]"} flex-col`}>
+                {/* Navbar  */}
+                <Navbar toggle={toggle} setToggle={setToggle}></Navbar>
+                <div className='w-full overflow-auto h-full bg-[#F5F5F5]'>
+                    <Outlet></Outlet>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MySuperDashboard
