a8cc8a603671baf70bc48802b2a87344c2bd6a31
SaurabhMagara <saurabhmagara@gmail.com>
2025-02-10 12:20:55 +0530

    blogs page created, blogs archive page created

---
+++ site/src/components/blogs/BlogHeroSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import blogheroimage from "../../assets/blogheroimage.jpg";
+import { CalendarDays, MessageSquareText } from 'lucide-react';
+
+const BlogHeroSection = () =>{
+    return (
+        <div className="relative pb-10  md:pb-0 md:min-h-[600px] w-full">
+            {/* Background Image with Overlay */}
+            <div
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+                style={{
+                    backgroundImage: `url(${blogheroimage})`,
+                }}
+            >
+                {/* Dark Overlay with gradient */}
+                <div className="absolute inset-0 bg-gradient-to-b from-black/50 to-black/70"></div>
+            </div>
+
+            {/* Main Content */}
+            <div className="relative z-10 flex flex-col items-center justify-center px-6 pt-32 lg:px-12 md:pt-12 md:h-[600px]">
+                {/* Tag */}
+                <span className="bg-green-400 text-white px-4 py-1 rounded-lg text-sm mb-8">
+                    SEO & DIGITAL MARKETING
+                </span>
+
+                {/* Heading */}
+                <h1 className="text-3xl md:text-4xl lg:text-5xl text-center  max-w-4xl leading-tight text-transparent bg-gradient-to-r from-blue-400 to-green-400 bg-clip-text">
+                    The Ultimate Guide to SEO: Boost Your Website's Visibility and Traffic
+                </h1>
+
+                {/* Meta Information */}
+                <div className="flex items-center space-x-4 mt-8 text-white/80">
+                    <div className="flex items-center gap-1">
+                        <CalendarDays className='text-[#BAFE6D]' />
+                        <span>June 21, 2024</span>
+                    </div>
+                    <div className="flex items-center gap-1">
+                        <MessageSquareText className='text-[#BAFE6D]' />
+                        <span>No Comments</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BlogHeroSection;
