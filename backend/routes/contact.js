b5e881141d62eacedf513932f5705325e2405eec
Yash <pyash7452@gmail.com>
2025-02-10 17:20:20 +0530

    get all consulation contacts

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-import { allContacts } from "../Controller/contactController.js";
+import { allContacts ,getAllContacts} from "../Controller/contactController.js";
 
 const router = express.Router();
 
 router.post("/", allContacts);
+router.get("/getall",getAllContacts)
 
 export default router;
