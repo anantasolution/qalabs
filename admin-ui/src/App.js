bfd65714ce2740e57110758b4ce8422fd3de4cc0
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2025-02-10 11:23:27 +0530

    added setup for datagrid

--- admin-ui/src/App.js
+++ admin-ui/src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Consultancy from "./pages/Consultancy";
+import ContactUs from "./pages/ContactUs";
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+     <Router>
+        <Routes>
+           <Route path="/contactus" element={<ContactUs></ContactUs>}></Route>
+           <Route path="/consultant" element={<Consultant></Consultant>}></Route>
+        </Routes>
+     </Router>
   );
 }
 
---
+++ admin-ui/src/data/Consultant.js
@@ -0,0 +1,1 @@
+//Wrire column data
