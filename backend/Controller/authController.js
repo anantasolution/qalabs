0756be70254890fa5acaf8e0e635eb583214c140
Yash <pyash7452@gmail.com>
2025-02-10 18:19:34 +0530

    login

--- backend/Controller/authController.js
+++ backend/Controller/authController.js
@@ -1,6 +1,8 @@
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcrypt";
 import Admin from "../models/ADMIN.js";
 import Loginmapping from "../models/LOGINMAPPING.js";
+import jwt from 'jsonwebtoken';
+
 
 export const createAdmin = async (req, res) => {
   try {
@@ -29,9 +31,8 @@ export const createAdmin = async (req, res) => {
     }
 
     // Hash password
-    const salt = bcryptjs.genSaltSync(10);
-    const hash = bcryptjs.hashSync(password, salt);
-
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
     // Create new Admin
     const newAdmin = new Admin({ username, email, mobileno, password: hash });
     await newAdmin.save();
@@ -82,3 +83,53 @@ const validateAdmin = (username, email, password, mobileno) => {
 
   return { isValid: errors.length === 0, errors };
 };
+
+
+export const loginAdmin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please provide email and password" });
+    }
+
+    const user = await Loginmapping.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid email address" });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      console.log("Password comparison failed");
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        mongoid: user.mongoid,
+        email: user.email,
+        user_type: user.user_type,
+      },
+      process.env.JWT_SECRET || "defaultSecretKey",
+      { expiresIn: "30d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        mongoid: user.mongoid,
+        email: user.email,
+        user_type: user.user_type,
+        status: user.status,
+      },
+    });
+
+  } catch (err) {
+    next(err);
+  }
+};
+
+
