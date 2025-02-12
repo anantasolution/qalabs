573959103a6f65654d4621b5d0e01e6e5e886aa3
mesuriyavivek7 <vivekmesuriya6@gmail.com>
2025-02-12 12:02:38 +0530

    done

--- admin-ui/src/pages/Categories.jsx
+++ admin-ui/src/pages/Categories.jsx
@@ -50,9 +50,8 @@ const Categories = () => {
 
   return (
     <div className="h-full w-full bg-gray-100 flex flex-col">
-      <div className="">
         <Breadcrumbs />
-      </div>
+      
       <div className="p-6 h-full w-full">
         <div className="p-6 bg-white h-full rounded-md">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
